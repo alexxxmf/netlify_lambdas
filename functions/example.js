9450571f6ee562482b1a840fa99b1c49d681d292
alejandro <amoro@alumni.unav.es>
2020-11-08 09:38:38 +0100

    change

--- functions/example.js
+++ functions/example.js
@@ -1,8 +1,4 @@
 exports.handler = function (event, context, callback) {
-  const body = JSON.parse(event.body);
-
-  console.log('body', body);
-
   return {
     statusCode: 200,
     body: 'success',
